2c280e6e278d0e853f18247f2cf25ff4ff9b54aa
EmreKiziltoprak <45095840+EmreKiziltoprak@users.noreply.github.com>
2022-11-02 11:19:52 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Data-grid-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-4dxu49)
